9c858ea9eef39394ce54eac3ea075943dfc1d8b4
Ruderson Florentino <rudersonvf@gmail.com>
2023-09-18 00:02:16 -0300

    Projection

--- backend/src/main/java/com/rudersonvf/dslist/services/GameService.java
+++ backend/src/main/java/com/rudersonvf/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.rudersonvf.dslist.services;
 import com.rudersonvf.dslist.dto.GameDTO;
 import com.rudersonvf.dslist.dto.GameMinDTO;
 import com.rudersonvf.dslist.entities.Game;
+import com.rudersonvf.dslist.projections.GameMinProjection;
 import com.rudersonvf.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,11 @@ public class GameService {
     public GameDTO findById(Long id){
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
